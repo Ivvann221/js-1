8a00cb3237471399562ebb4921a3c123d2e0022e
Hiko1234 <94067148+Ivvann221@users.noreply.github.com>
2023-01-15 13:18:02 +0200

    js-2

--- src/js/script.js
+++ src/js/script.js
@@ -1,45 +1,21 @@
-// alert('Ostap');
-
-// console.log('Ostap');
-
-// let name = "ostap";
-// let surname = "doe";
-// let middleName = 'patric';
-
-// Array
-// let age = [12, 13, 22, 17]
-// alert(surname[1])
-
-// const personalData = surname + ' ' + name[0] + '.' + ' ' + middleName[0] + '.';
-// console.log(personalData);
-
-// let num1 = 2;
-// let num2 = 3;
-
-// const result = num1 == num2;
-// console.log('result', result);
-
-// let arraySome = ['test', 13, true, 12.56];
-// arraySome.push('test2') - додає в кінець
-//  arraySome.unshift('test2'); - добавляє в початок
-// arraySome.pop('test2'); - забирає ост елемент
-// const position = arraySome.indexOf(13); - шукає індекс елемента
-
-// console.log('Lenght', arraySome.length); - довжина масива
-
-// let arraySome = ['test', 13, true, 12.56]; 
-//  const position = arraySome.indexOf(13); -шукає елемент
-//  arraySome.splice(position, 1); -видаляє елемент
-//  console.log('Lenght', arraySome);
+let personalData = {
+    name: "john",
+    surName: "patric",
+    lastName: "jonson",
+    address: {
+        city: "Dro",
+        street: "ran",
+        number: 11
+    },
+    age: [1, 3, 4]
+}
+console.log(personalData.name);
+console.log(personalData.address.street);
+console.log(personalData.age[1]);
 
 
-let arraySome = [2, 13, 3, 12.56]; 
- const position = arraySome.indexOf(13);
- arraySome.splice(position, 1);
- console.log('Lenght', arraySome);
+const size = prompt("Введіть розмір");
+console.log(typeof size);
 
- if (position >= 0){
-  console.log(`Число ${13}`);
- }else{
-  console.log(`Число ${13} dfg` );
- }
+personalData.size = size;
+console.log(personalData);
