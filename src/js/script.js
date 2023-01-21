a227459d10cefcc106a2c9f476d96764fbc9f03e
Hiko1234 <94067148+Ivvann221@users.noreply.github.com>
2023-01-21 13:10:43 +0200

    1

--- src/js/script.js
+++ src/js/script.js
@@ -1,45 +1,103 @@
-// alert('Ostap');
+let fruits = [
+    "apple",
+    "banana",
+    "orange",
+    "pear",
+    "grape",
+    "pineapple",
+    "strawberry",
+    "watermelon",
+    "kiwi",
+    "mango",
+    "peach",
+    "lemon",
+    "lime",
+    "blueberry",
+    "raspberry",
+    "blackberry",
+    "apricot",
+    "cherry",
+    "coconut",
+    "fig",
+    "pomegranate",
+    "plum",
+    "nectarine",
+    "persimmon",
+    "tangerine",
+    "papaya",
+    "guava",
+    "jackfruit",
+    "lychee",
+    "starfruit",
+    "cantaloupe",
+    "honeydew",
+    "mangosteen",
+    "pawpaw",
+    "durian",
+    "tamarind",
+    "mulberry",
+    "currant",
+    "gooseberry",
+    "rambutan",
+    "longan",
+    "dragonfruit",
+    "clementine",
+    "satsuma",
+    "mandarin",
+    "tangelo",
+    "yuzu",
+    "nectarine",
+    "persimmon",
+    "tangerine",
+    "papaya",
+    "guava",
+    "jackfruit",
+    "lychee",
+    "starfruit",
+    "cantaloupe",
+    "honeydew",
+    "mangosteen",
+    "pawpaw",
+    "durian",
+    "tamarind",
+    "mulberry",
+    "currant",
+    "gooseberry",
+    "rambutan",
+    "longan",
+    "dragonfruit",
+    "clementine",
+    "satsuma",
+    "mandarin",
+    "tangelo",
+    "yuzu",
+    "nectarine",
+    "persimmon",
+    "tangerine",
+    "papaya",
+    "guava",
+    "jackfruit",
+    "lychee",
+    "starfruit",
+    "cantaloupe",
+    "honeydew",
+    "mangosteen",
+    "pawpaw",
+    "durian",
+    "tamarind",
+    "mulberry",
+    "currant",
+    "gooseberry",
+    "rambutan",
+  ];
 
-// console.log('Ostap');
-
-// let name = "ostap";
-// let surname = "doe";
-// let middleName = 'patric';
-
-// Array
-// let age = [12, 13, 22, 17]
-// alert(surname[1])
-
-// const personalData = surname + ' ' + name[0] + '.' + ' ' + middleName[0] + '.';
-// console.log(personalData);
-
-// let num1 = 2;
-// let num2 = 3;
-
-// const result = num1 == num2;
-// console.log('result', result);
-
-// let arraySome = ['test', 13, true, 12.56];
-// arraySome.push('test2') - додає в кінець
-//  arraySome.unshift('test2'); - добавляє в початок
-// arraySome.pop('test2'); - забирає ост елемент
-// const position = arraySome.indexOf(13); - шукає індекс елемента
-
-// console.log('Lenght', arraySome.length); - довжина масива
-
-// let arraySome = ['test', 13, true, 12.56]; 
-//  const position = arraySome.indexOf(13); -шукає елемент
-//  arraySome.splice(position, 1); -видаляє елемент
-//  console.log('Lenght', arraySome);
-
-
-let arraySome = [2, 13, 3, 12.56]; 
- const position = arraySome.indexOf(13);
- arraySome.splice(position, 1);
- console.log('Lenght', arraySome);
-
- if (position >= 0){
-  console.log(`Число ${13}`);
- }else{
-  console.log(`Число ${13} dfg` );
- }
+  if (search <= 0){
+    console.log(`Помилка`);
+   
+ 
+  }else{
+    const search = fruits.indexOf('kiwi');
+    const element = fruits[-1];
+    console.log(`I like ${element}`);
+  }   
+ 
