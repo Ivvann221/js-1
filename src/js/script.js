67cef04243a74c9afabc36e39a07d4c96d88c411
Hiko1234 <94067148+Ivvann221@users.noreply.github.com>
2023-01-22 14:42:50 +0200

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,63 @@
-// // 1. Persinal data of users
-const usersData = [
+// // // 1. Persinal data of users
+// const usersData = [
+//   {
+//     name: "John",
+//     age: 25,
+//     surname: "Doe",
+//     fatherName: "Smith",
+//   },
+//   {
+//     name: "Jane",
+//     age: 30,
+//     surname: "Doe",
+//     fatherName: "Sarah",
+//   },
+//   {
+//     name: "Jack",
+//     age: 28,
+//     surname: "Doe",
+//     fatherName: "John",
+//   },
+//   {
+//     name: "Jill",
+//     age: 32,
+//     surname: "Doe",
+//     fatherName: "Monica",
+//   },
+// ];
+// let counter = 0;
+
+// function getInitialsUser(surname, name, fatherName) {
+//     counter++;
+//     if(counter <= 3){
+//         return surname + " " + name[0] + "." + fatherName[0] + ".";
+//     }else{
+//         return "Functiom is called more than 3 times"
+//     } 
+// }
+
+// function displayAlertInputText(text){
+//     alert(text);
+// }
+
+
+// usersData.forEach((user) => {
+//   const userInitials = getInitialsUser(
+//     user.surname,
+//     user.name,
+//     user.fatherName
+//   );
+//   console.log(userInitials + " " + user.age);
+// });
+
+// // Input data to diplay in alert
+// const inputText = prompt("Enter text to display in alert");
+// displayAlertInputText(inputText);
+
+ const usersData = [
   {
     name: "John",
-    age: 25,
+    age: 15,
     surname: "Doe",
     fatherName: "Smith",
   },
@@ -25,31 +80,27 @@ const usersData = [
     fatherName: "Monica",
   },
 ];
-let counter = 0;
 
-function getInitialsUser(surname, name, fatherName) {
-    counter++;
-    if(counter <= 3){
-        return surname + " " + name[0] + "." + fatherName[0] + ".";
+ function getInitialsAge(age) {
+    if(age >= 18){
+        return true;
     }else{
-        return "Functiom is called more than 3 times"
-    } 
+        return false;
+    }
 }
-
-function displayAlertInputText(text){
-    alert(text);
-}
-
-
+const variabl = getInitialsAge(18);
 usersData.forEach((user) => {
-  const userInitials = getInitialsUser(
-    user.surname,
-    user.name,
-    user.fatherName
-  );
-  console.log(userInitials + " " + user.age);
-});
+    if(getInitialsAge(user.age)) {
+      console.log("повнолітній" + " " + user.age);
+       }else{
+        console.log("неповнолітній" + " " + user.age);
+       }
+    });
 
-// Input data to diplay in alert
-const inputText = prompt("Enter text to display in alert");
-displayAlertInputText(inputText);
+    let counter = 0;
+    usersData.forEach(function (item) {
+  if (item[0] == user.age) {
+    console.log(item);
+    counter++;
+  }
+});
